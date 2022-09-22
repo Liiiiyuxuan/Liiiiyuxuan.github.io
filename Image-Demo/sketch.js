6630cfb9697622822ab5ceebd9bf69c4dd9b094c
Liiiiyuxuan <liiiiyuxuan@gmail.com>
2022-09-22 12:05:16 -0600

    Image Demo

---
+++ Image-Demo/sketch.js
@@ -0,0 +1,31 @@
+// Image Demo
+// Eason
+// Sept 22, 2022
+
+let fishImage;
+let scalar = 1;
+
+function preload() {
+  fishImage = loadImage("Fish.webp");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  image(fishImage, mouseX, mouseY, fishImage.width*scalar, fishImage.height*scalar);
+
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    scalar ++;
+  };
+
+  if (keyCode === DOWN_ARROW) {
+    scalar --;
+  };
+}
