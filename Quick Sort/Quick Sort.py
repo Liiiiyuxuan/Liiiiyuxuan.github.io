74a8f75ec0094083a6ce89a4239d4060ae3212ee
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-09-27 12:15:57 -0600

    Create Quick Sort.py

---
+++ Quick Sort/Quick Sort.py
@@ -0,0 +1,10 @@
+listOfNumbers = []
+askForNumbers = True
+
+if askForNumbers:
+    if 
+    inputNumber = input("Give me some arbituary numbers (space bar to terminate the ):   ")
+    listOfNumbers.append(int(inputNumber))
+    
+
+print(listOfNumbers)
