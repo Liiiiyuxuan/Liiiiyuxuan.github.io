09796471558d289c63d157f82815348eabe4bc02
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-09-27 20:02:09 -0600

    Update Quick Sort.py

--- Quick Sort/Quick Sort.py
+++ Quick Sort/Quick Sort.py
@@ -1,10 +1,33 @@
-listOfNumbers = []
-askForNumbers = True
+from re import A
+from numpy import number
+import keyboard
 
-if askForNumbers:
-    if 
-    inputNumber = input("Give me some arbituary numbers (space bar to terminate the ):   ")
+
+listOfNumbers = []
+askForInput = True
+
+
+def askForNumbers():
+    inputNumber = input("Give me some arbituary numbers (space bar to terminate):   ")
     listOfNumbers.append(int(inputNumber))
+
+def askForArray():
+    inputArray = input("Give me the array waiting to be sorted:   ")
+    listOfNumbers = inputArray
+
     
+if askForInput:
+    numberOrArray = input("Numbers or Array? (1 for numbers / 2 for array):   ")
+    if numberOrArray == 1:
+        while askForInput:
+            askForNumbers()
+            if keyboard.is_pressed("q"):
+                break
+
+    elif numberOrArray == 2:
+        while askForInput:
+            askForArray()
+
+
 
 print(listOfNumbers)
