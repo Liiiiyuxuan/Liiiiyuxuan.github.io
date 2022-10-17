e5c5fcd97bc60af566a7cfb03f701092324f7e46
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-17 12:16:15 -0600

    Update sketch.js

--- Chess Board/sketch.js
+++ Chess Board/sketch.js
@@ -1,4 +1,8 @@
-let index = 8;
+let numberOfCellsEachSide = 8;
+let pawn, rook, knight, bishop, king, queen;
+let pawnImg, rookImg, knightImg, bishopImg, kingImg, queenImg;
+let blackChecked, whiteChecked;
+let pieceMoveable;
 
 function setup() {
   createCanvas(min(windowWidth, windowHeight), min(windowWidth, windowHeight));
@@ -10,11 +14,11 @@ function draw() {
 }
 
 function drawChessboard() {
-  let cellSideLength = min(windowWidth, windowHeight) / index;
+  let cellSideLength = min(windowWidth, windowHeight) / numberOfCellsEachSide;
   let isWhite = true;
 
-  for (let i = 0; i < index; i++) {
-    for (let j = 0; j < index; j++) {
+  for (let i = 0; i < numberOfCellsEachSide; i++) {
+    for (let j = 0; j < numberOfCellsEachSide; j++) {
       isWhite === true ? fill("white") : fill("black");
 
       noStroke();
