624c842d1c22e01f818e896695ebb55392c346c5
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-17 21:59:59 -0600

    Update sketch.js

--- Chess Board/sketch.js
+++ Chess Board/sketch.js
@@ -1,13 +1,26 @@
 let canvasSize = 640;
 let coffee = '#4B3619';
+let lightgray = 'lightgray'
 
 let numberOfCellsEachSide = 8;
-let blackPawn, blackRook, blackKnight, blackBishop, blackKing, blackQueen;
-let whitePawn, whiteRook, whiteKnight, whiteBishop, whiteKing, whiteQueen;
+
+let blackPawn1, blackPawn2, blackPawn3,   blackPawn4,   blackPawn5,   blackPawn6,   blackPawn7, blackPawn8;
+let whitePawn1, whitePawn2, whitePawn3,   whitePawn4,   whitePawn5,   whitePawn6,   whitePawn7, whitePawn8;
+let blackRook1, blackRook2, blackKnight1, blackKnight2, blackBishop1, blackBishop2, blackKing,  blackQueen;
+let whiteRook1, whiteRook2, whiteKnight1, whiteKnight2, whiteBishop1, whiteBishop2, whiteKing,  whiteQueen;
+
 let blackPawnImg, blackRookImg, blackKnightImg, blackBishopImg, blackKingImg, blackQueenImg;
 let whitePawnImg, whiteRookImg, whiteKnightImg, whiteBishopImg, whiteKingImg, whiteQueenImg;
-let pieceWidth = 75;
-let pieceHeight = 100;
+
+let pawnWidth = 75;
+let pawnHeight = 100;
+let rookWidth = 75;
+let rookHeight = 88;
+let knightWidth = 72;
+let knightHeight = 88;
+let bishopWidth = 71;
+let bishopHeight = 81;
+
 let blackChecked, whiteChecked;
 let pieceMoveable;
 
@@ -37,23 +50,44 @@ function draw() {
   background(220);
   drawChessboard();
 
-  image(whitePawnImg, canvasSize / 8, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(whitePawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
+  // white pawn
+  whitePawn1 = image(whitePawnImg, canvasSize / 8 * 0, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn2 = image(whitePawnImg, canvasSize / 8 * 1, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn3 = image(whitePawnImg, canvasSize / 8 * 2, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn4 = image(whitePawnImg, canvasSize / 8 * 3, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn5 = image(whitePawnImg, canvasSize / 8 * 4, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn6 = image(whitePawnImg, canvasSize / 8 * 5, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn7 = image(whitePawnImg, canvasSize / 8 * 6, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  whitePawn8 = image(whitePawnImg, canvasSize / 8 * 7, canvasSize / 2 + 2 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
 
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
-  image(blackPawnImg, canvasSize / 2, canvasSize / 2, pieceWidth, pieceHeight);
+  whiteRook1 = image(whiteRookImg,   canvasSize / 8 * 0, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, rookWidth, rookHeight);
+  image(whiteKnightImg, canvasSize / 8 * 1, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, knightWidth, knightHeight);
+  image(whiteBishopImg, canvasSize / 8 * 2, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, bishopWidth, bishopHeight);
+  image(whiteQueenImg,  canvasSize / 8 * 3, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  image(whiteKingImg,   canvasSize / 8 * 4, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  image(whiteBishopImg, canvasSize / 8 * 5, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, bishopWidth, bishopHeight);
+  image(whiteKnightImg, canvasSize / 8 * 6, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, knightWidth, knightHeight);
+  image(whiteRookImg,   canvasSize / 8 * 7, canvasSize / 2 + 3 * canvasSize / numberOfCellsEachSide, rookWidth, rookHeight);
+
+  // black pawn
+  blackPawn1 = image(blackPawnImg, canvasSize / 8 * 0, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn2 = image(blackPawnImg, canvasSize / 8 * 1, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn3 = image(blackPawnImg, canvasSize / 8 * 2, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn4 = image(blackPawnImg, canvasSize / 8 * 3, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn5 = image(blackPawnImg, canvasSize / 8 * 4, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn6 = image(blackPawnImg, canvasSize / 8 * 5, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn7 = image(blackPawnImg, canvasSize / 8 * 6, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  blackPawn8 = image(blackPawnImg, canvasSize / 8 * 7, canvasSize / 2 - 3 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+
+  image(blackRookImg,   canvasSize / 8 * 0, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, rookWidth, rookHeight);
+  image(blackKnightImg, canvasSize / 8 * 1, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, knightWidth, knightHeight);
+  image(blackBishopImg, canvasSize / 8 * 2, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, bishopWidth, bishopHeight);
+  image(blackQueenImg,  canvasSize / 8 * 3, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  image(blackKingImg,   canvasSize / 8 * 4, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, pawnWidth, pawnHeight);
+  image(blackBishopImg, canvasSize / 8 * 5, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, bishopWidth, bishopHeight);
+  image(blackKnightImg, canvasSize / 8 * 6, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, knightWidth, knightHeight);
+  image(blackRookImg,   canvasSize / 8 * 7, canvasSize / 2 - 4 * canvasSize / numberOfCellsEachSide, rookWidth, rookHeight);
+
 }
 
 function drawChessboard() {
@@ -62,7 +96,7 @@ function drawChessboard() {
 
   for (let i = 0; i < numberOfCellsEachSide; i++) {
     for (let j = 0; j < numberOfCellsEachSide; j++) {
-      isWhite === true ? fill("white") : fill(coffee);
+      isWhite === true ? fill(lightgray) : fill(coffee);
 
       noStroke();
       rect(i*cellSideLength, j*cellSideLength, cellSideLength, cellSideLength);
