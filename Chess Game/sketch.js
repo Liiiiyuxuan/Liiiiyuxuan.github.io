4e2dea0c8637be69d9e1734fbb346ccdc04b44f1
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-18 22:03:03 -0600

    Update sketch.js

--- Chess Game/sketch.js
+++ Chess Game/sketch.js
@@ -1,6 +1,11 @@
 let chessboardSize = 800;
 let columns = 8;
 let rows = 8;
+let counter = 0;
+
+let colourList = ["white", "black"];
+let numberList = ['8', '7', '6', '5', '4', '3', '2', '1'];
+let letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 let theBoard = [
   ['', '', '', '', '', '', '', ''],
@@ -11,14 +16,47 @@ let theBoard = [
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', ''],
   ['', '', '', '', '', '', '', '']
-]
+];
 
 
 function setup() {
-  background('black')
+  createCanvas(chessboardSize, chessboardSize);
 }
 
 function draw() {
-  background(220);
+  drawChessboard();
+  designateCoordinates();
+  console.log(theBoard);
 }
 
+function drawChessboard() {
+  for (let i = 0; i <= 8; i++) {
+    for (let j = 0; j <= 8; j++) {
+      fill(colourList[counter]);
+      rect(i*chessboardSize/rows, j*chessboardSize/columns, chessboardSize/rows, chessboardSize/columns);
+      
+      if (counter === 0) {
+        counter ++;
+      } else if (counter === 1) {
+        counter --;
+      }
+    }
+  }
+  counter--;
+}
+
+function designateCoordinates() {
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < columns; i++) {
+      property = {
+        x: letterList[j],
+        y: numberList[i],
+      }
+      theBoard[i][j] = property;
+    }
+  }
+}
+
+function setUpPieces() {
+
+}
