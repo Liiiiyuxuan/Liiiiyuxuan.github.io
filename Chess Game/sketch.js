99758b026d183938b8dc7f10d78c4b09990875bb
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-19 15:35:33 -0600

    Update sketch.js

--- Chess Game/sketch.js
+++ Chess Game/sketch.js
@@ -1,4 +1,4 @@
-let chessboardSize = 800;
+let chessboardSize = 720;
 let columns = 8;
 let rows = 8;
 let counter = 0;
@@ -23,6 +23,18 @@ let blackPawnImg, blackRookImg, blackKnightImg, blackBishopImg, blackQueenImg, b
 
 function preload() {
   whitePawnImg = loadImage('white.pawn.png');
+  whiteRookImg = loadImage('white.rook.png');
+  whiteKnightImg = loadImage('white.knight.png');
+  whiteBishopImg = loadImage('white.Bishop.png');
+  whiteQueenImg = loadImage('white.Queen.png');
+  whiteKingImg = loadImage('white.King.png');
+
+  blackPawnImg = loadImage('black.pawn.png');
+  blackRookImg = loadImage('black.rook.png');
+  blackKnightImg = loadImage('black.knight.png');
+  blackBishopImg = loadImage('black.Bishop.png');
+  blackQueenImg = loadImage('black.Queen.png');
+  blackKingImg = loadImage('black.King.png');
 }
 
 function setup() {
@@ -32,7 +44,8 @@ function setup() {
 function draw() {
   drawChessboard();
   designateCoordinates();
-  image(whitePawnImg, 400, 400, 100, 100);
+  setUpPieces();
+  console.log(theBoard);
 }
 
 function drawChessboard() {
@@ -57,6 +70,7 @@ function designateCoordinates() {
       property = {
         x: letterList[j],
         y: numberList[i],
+        piece: 'none',
       }
       theBoard[i][j] = property;
     }
@@ -64,5 +78,64 @@ function designateCoordinates() {
 }
 
 function setUpPieces() {
+  // setting up white pawns
+  for (let i = 0; i < rows; i++) {
+    image(whitePawnImg, chessboardSize / rows * i, chessboardSize / columns * 6, chessboardSize / rows, chessboardSize / columns);
+    theBoard[6][i].property = 'whitePawn';
+  }
 
+  // setting up white rooks
+  image(whiteRookImg, chessboardSize / rows * 0, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  image(whiteRookImg, chessboardSize / rows * 7, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  theBoard[7][0].property = 'whiteRook';
+  theBoard[7][7].property = 'whiteRook';
+
+  // setting up white knights
+  image(whiteKnightImg, chessboardSize / rows * 1, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  image(whiteKnightImg, chessboardSize / rows * 6, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  theBoard[7][1].property = 'whiteKnight';
+  theBoard[7][6].property = 'whiteKnight';
+
+  // setting up white bishops
+  image(whiteBishopImg, chessboardSize / rows * 2, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  image(whiteBishopImg, chessboardSize / rows * 5, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  theBoard[7][2].property = 'whiteBishop';
+  theBoard[7][5].property = 'whiteBishop';
+
+  // setting up white queen and king
+  image(whiteQueenImg, chessboardSize / rows * 3, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  image(whiteKingImg,  chessboardSize / rows * 4, chessboardSize / columns * 7, chessboardSize / rows, chessboardSize / columns);
+  theBoard[7][3].property = 'whiteQueen';
+  theBoard[7][4].property = 'whiteKing';
+
+
+  // setting up black pawns
+  for (let i = 0; i < rows; i++) {
+    image(blackPawnImg, chessboardSize / rows * i, chessboardSize / columns * 1, chessboardSize / rows, chessboardSize / columns);
+    theBoard[1][i].property = 'blackPawn';
+  }
+
+  // setting up black rooks
+  image(blackRookImg, chessboardSize / rows * 0, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  image(blackRookImg, chessboardSize / rows * 7, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  theBoard[0][0].property = 'blackRook';
+  theBoard[0][7].property = 'blackRook';
+
+  // setting up black knights
+  image(blackKnightImg, chessboardSize / rows * 1, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  image(blackKnightImg, chessboardSize / rows * 6, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  theBoard[0][1].property = 'blackKnight';
+  theBoard[0][6].property = 'blackKnight';
+
+  // setting up black bishops
+  image(blackBishopImg, chessboardSize / rows * 2, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  image(blackBishopImg, chessboardSize / rows * 5, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  theBoard[0][2].property = 'blackBishop';
+  theBoard[0][5].property = 'blackBishop';
+
+  // setting up black queen and king
+  image(blackQueenImg, chessboardSize / rows * 3, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  image(blackKingImg,  chessboardSize / rows * 4, chessboardSize / columns * 0, chessboardSize / rows, chessboardSize / columns);
+  theBoard[0][3].property = 'blackQueen';
+  theBoard[0][4].property = 'blackKing';
 }
