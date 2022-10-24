271d27fd39e16afe42e91b0ef19e1195c28c8683
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-23 23:21:53 -0600

    Update sketch.js

--- Chess Game New Version/sketch.js
+++ Chess Game New Version/sketch.js
@@ -181,48 +181,82 @@ function movePawn() {
       if (y === 6 && theBoard[y][x].colour === 'white' && theBoard[y][x].piece === 'pawn' && theBoard[y][x].selected) {
         initializingAvailability();
         fill(25, 255, 25, 125);
-        if (theBoard[y-1][x].piece === 'none') {
+        if (y - 1 >= 0 && theBoard[y-1][x].piece === 'none') {
           rect(x * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-          initializingAvailability();
           if (y-2 > 0 && theBoard[y-2][x].piece === 'none') {
             rect(x * (boardSize / columns), (y-2) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-            initializingAvailability();
             theBoard[y-2][x].available = true;
           }
           theBoard[y-1][x].available = true;
+        }
+        if (y - 1 >= 0 && x - 1 >= 0 && theBoard[y-1][x-1].colour === 'black') {
+          fill(255, 25, 25, 125);
+          rect((x-1) * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y-1][x-1].available = true;
+        }
+        if (y - 1 >= 0 && x + 1 < columns && theBoard[y-1][x+1].colour === 'black') {
+          fill(255, 25, 25, 125);
+          rect((x+1) * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y-1][x+1].available = true;
         }
       }
       else if (y !== 6 && theBoard[y][x].colour === 'white' && theBoard[y][x].piece === 'pawn' && theBoard[y][x].selected) {
         initializingAvailability();
         fill(25, 255, 25, 125);
-        if (theBoard[y-1][x].piece === 'none') {
+        if (y - 1 >= 0 && theBoard[y-1][x].piece === 'none') {
           rect(x * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-          initializingAvailability();
           theBoard[y-1][x].available = true;
+        }
+        if (y - 1 >= 0 && x - 1 >= 0 && theBoard[y-1][x-1].colour === 'black') {
+          fill(255, 25, 25, 125);
+          rect((x-1) * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y-1][x-1].available = true;
+        }
+        if (y - 1 >= 0 && x + 1 < columns && theBoard[y-1][x+1].colour === 'black') {
+          fill(255, 25, 25, 125);
+          rect((x+1) * (boardSize / columns), (y-1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y-1][x+1].available = true;
         }
       }
 
       if (y === 1 && theBoard[y][x].colour === 'black' && theBoard[y][x].piece === 'pawn' && theBoard[y][x].selected) {
         initializingAvailability();
         fill(25, 255, 25, 125);
-        if (theBoard[y+1][x].piece === 'none') {
+        if (y + 1 < rows && theBoard[y+1][x].piece === 'none') {
           rect(x * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-          initializingAvailability();
           if (y+2 < rows && theBoard[y+2][x].piece === 'none') {
             rect(x * (boardSize / columns), (y+2) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-            initializingAvailability();
             theBoard[y+2][x].available = true;
           }
           theBoard[y+1][x].available = true;
+        }
+        if (y + 1 >= 0 && x - 1 >= 0 && theBoard[y+1][x-1].colour === 'white') {
+          fill(255, 25, 25, 125);
+          rect((x-1) * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y+1][x-1].available = true;
+        }
+        if (y + 1 >= 0 && x + 1 < columns && theBoard[y+1][x+1].colour === 'white') {
+          fill(255, 25, 25, 125);
+          rect((x+1) * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y+1][x+1].available = true;
         }
       }
       else if (y !== 1 && theBoard[y][x].colour === 'black' && theBoard[y][x].piece === 'pawn' && theBoard[y][x].selected) {
         initializingAvailability();
         fill(25, 255, 25, 125);
-        if (theBoard[y+1][x].piece === 'none') {
+        if (y + 1 < rows && theBoard[y+1][x].piece === 'none') {
           rect(x * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
-          initializingAvailability();
           theBoard[y+1][x].available = true;
+        }
+        if (y + 1 >= 0 && x - 1 >= 0 && theBoard[y+1][x-1].colour === 'white') {
+          fill(255, 25, 25, 125);
+          rect((x-1) * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y+1][x-1].available = true;
+        }
+        if (y + 1 >= 0 && x + 1 < columns && theBoard[y+1][x+1].colour === 'white') {
+          fill(255, 25, 25, 125);
+          rect((x+1) * (boardSize / columns), (y+1) * (boardSize / rows), (boardSize / columns), (boardSize / rows));
+          theBoard[y+1][x+1].available = true;
         }
       }
     }
