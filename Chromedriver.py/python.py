116622cd92c840bfbc098501a0b4c80e3c6ed718
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-10-28 13:01:03 -0600

    Update python.py

--- Chromedriver.py/python.py
+++ Chromedriver.py/python.py
@@ -1,2 +1,9 @@
-import selenium
+from selenium import webdriver
 
+driverPath = 'chromedriver.exe'
+
+driver = webdriver.Chrome(driverPath)
+
+driver.get('http://www.google.com')
+
+print(driver.page_source)
