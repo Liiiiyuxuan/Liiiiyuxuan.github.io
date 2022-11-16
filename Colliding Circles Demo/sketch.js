a73f6d1fddadd9fdaa77473bc5d86faddd3710a6
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-11-16 11:58:13 -0600

    colliding circle using oop

--- Colliding Circles Demo/sketch.js
+++ Colliding Circles Demo/sketch.js
@@ -2,74 +2,69 @@ let theCircles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  theCircles.push(spawnBall(windowWidth / 2, windowHeight / 2));
 }
 
 function draw() {
-  background(220);
+  background(100);
 
-  // move
   for (let i = 0; i < theCircles.length; i++) {
-    theCircles[i].x += theCircles[i].dx;
-    theCircles[i].y += theCircles[i].dy;
-
-    // collision check
+    theCircles[i].move();
+    theCircles[i].display();
     for (let j = 0; j < theCircles.length; j++) {
-      if(i !== j && checkCollision(theCircles[i], theCircles[j])) {
-        let tempDx = theCircles[i].dx;
-        let tempDy = theCircles[i].dy;
-
-        theCircles[i].dx = theCircles[j].dx;
-        theCircles[i].dy = theCircles[j].dy;
-        theCircles[j].dx = tempDx;
-        theCircles[j].dy = tempDy;
-      }
+      theCircles[i].collisionCheck(theCircles[j]);
     }
-
-    // left-right edges
-    if (theCircles[i].x > width - theCircles[i].radius || theCircles[i].x < 0 + theCircles[i].radius) {
-      theCircles[i].dx *= -1;
-    }
-
-    // top-bottom edge
-    if (theCircles[i].y > height - theCircles[i].radius || theCircles[i].y < 0 + theCircles[i].radius) {
-      theCircles[i].dy *= -1;
-    }
-  }
-
-  // display
-  for (let thisCircle of theCircles) {
-    fill(thisCircle.theColour);
-    noStroke();
-    circle(thisCircle.x, thisCircle.y, thisCircle.radius * 2);
-  }
-}
-
-function checkCollision(ball1, ball2) {
-  let distanceBetween = dist(ball1.x, ball1.y, ball2.x, ball2.y);
-  let radiiSum = ball1.radius + ball2.radius;
-
-  if (distanceBetween > radiiSum) {
-    return false;
-  }
-  else {
-    return true;
   }
 }
 
 function mousePressed() {
-  theCircles.push(spawnBall(mouseX, mouseY));
+  theCircles.push(new Ball(mouseX, mouseY));
 }
 
-function spawnBall(tempX, tempY) {
-  let newBall = {
-    x: tempX,
-    y: tempY,
-    radius: random(25, 50),
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-    theColour: color(random(255), random(255), random(255), random(255)),
-  };
-  return newBall;
+
+
+class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = random(25, 50);
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
+    this.colour = color(random(255), random(255), random(255), random(255));
+  }
+
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    // left-right edges
+    if (this.x > width - this.radius || this.x < 0 + this.radius) {
+      this.dx *= -1;
+    }
+
+    // top-bottom edge
+    if (this.y > height - this.radius || this.y < 0 + this.radius) {
+      this.dy *= -1;
+    }
+  }
+
+  display() {
+    fill(this.colour);
+    noStroke();
+    circle(this.x, this.y, this.radius * 2);
+  }
+
+  collisionCheck(other) {
+    let distanceBetween = dist(this.x, this.y, other.x, other.y);
+    let radiiSum = this.radius + other.radius;
+  
+    if (distanceBetween < radiiSum) {
+      let tempDx = this.dx;
+      let tempDy = this.dy;
+
+      this.dx = other.dx;
+      this.dy = other.dy;
+      other.dx = tempDx;
+      other.dy = tempDy;
+    }
+  }
 }
