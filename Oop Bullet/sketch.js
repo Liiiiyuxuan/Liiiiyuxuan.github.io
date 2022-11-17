7c15c1a49e69d038684e1ed5842441f38f68dd19
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-11-17 11:59:39 -0600

    image for bullet oop

--- Oop Bullet/sketch.js
+++ Oop Bullet/sketch.js
@@ -1,7 +1,13 @@
+let bulletArray = [];
+
 let gunX;
 let gunY;
 
-let bulletArray = [];
+let bulletImg;
+
+function preload() {
+  bulletImg = loadImage('bullet.png');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,38 +16,46 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(215);
   
-  for (let i = 0; i < bulletArray.length; i++) {
-    bulletArray[i].move();
-    bulletArray[i].display();
+  for (let bullet of bulletArray) {
+    bullet.move();
+    bullet.display();
   }
 }
 
 function mousePressed() {
-  bulletArray.push(new Bullet(mouseX, mouseY));
+  bulletArray.push(new Bullet(mouseX, mouseY, bulletImg));
 }
 
 
 
 class Bullet {
-  constructor(clickX, clickY) {
+  constructor(clickX, clickY, theImg) {
     this.startX = gunX;
     this.startY = gunY;
     this.clickX = clickX;
     this.clickY = clickY;
     this.dx = (this.clickX - this.startX) / 10;
     this.dy = (this.clickY - this.startY) / 10;
+    this.img = theImg;
   }
 
   move() {
+    if (this.dx > 20 || this.dy > 10 || this.dx < -20 || this.dy < -10) {
+      this.dx *= 1/3;
+      this.dy *= 1/3;
+    }
+
     this.startX += this.dx;
     this.startY += this.dy;
   }
 
   display() {
-    fill('black');
-    noStroke();
-    circle(this.startX, this.startY, 5);
+    // fill('black');
+    // noStroke();
+    // circle(this.startX, this.startY, 5);
+    
+    image(this.img, this.startX, this.startY, 30, 25)
   }
 }
