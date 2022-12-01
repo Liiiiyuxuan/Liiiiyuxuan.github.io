9e6c6adb9a6aceffe547bf7227929b4c2d134cd3
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-12-01 12:17:47 -0600

    tailwind css

---
+++ Projects File/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'css_folder/grid.css',
+    'grid.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
