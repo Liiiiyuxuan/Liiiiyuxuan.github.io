e15b2c9ee1a35a98b9d7b8d4929147032bb72d85
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2022-12-19 11:53:06 -0600

    sierpinski triangle

---
+++ Sierpinski Triangle/sketch.js
@@ -0,0 +1,53 @@
+let triangleVertices = [
+  {x: 500, y: 50},
+  {x: 100, y: 600},
+  {x: 900, y: 600},
+];
+
+let theDepth = 0;
+let theColours = ['blue', 'red', 'green', 'white', 'yellow', 'purple', 'black', 'orange'];
+
+function setup() {
+  createCanvas(1000, 700);
+}
+
+function draw() {
+  background(220);
+  sierpinski(triangleVertices, theDepth);
+}
+
+function mousePressed() {
+  theDepth++;
+}
+
+function sierpinski(vertices, depth) {
+  fill(theColours[depth % 8]);
+  triangle(vertices[0].x, vertices[0].y, 
+           vertices[1].x, vertices[1].y, 
+           vertices[2].x, vertices[2].y);
+
+  if (depth > 0) {
+    sierpinski([vertices[0], 
+                getMidPt(vertices[0], vertices[1]),
+                getMidPt(vertices[0], vertices[2])],
+              depth - 1);
+    
+    sierpinski([vertices[1], 
+                getMidPt(vertices[0], vertices[1]),
+                getMidPt(vertices[1], vertices[2])],
+              depth - 1);
+
+    sierpinski([vertices[2], 
+                getMidPt(vertices[0], vertices[2]),
+                getMidPt(vertices[1], vertices[2])],
+              depth - 1);
+  }
+}
+
+function getMidPt(point1, point2) {
+  let x = 1/2 * (point1.x + point2.x);
+  let y = 1/2 * (point1.y + point2.y);
+  let MidPt = {x, y}
+
+  return MidPt;
+}
