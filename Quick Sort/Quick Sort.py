6f8992ce4857d93f97e7d1048a2e98328e5a340a
Liiiiyuxuan <113405136+Liiiiyuxuan@users.noreply.github.com>
2023-01-19 21:28:57 -0600

    still working on the quick sort

--- Quick Sort/Quick Sort.py
+++ Quick Sort/Quick Sort.py
@@ -1,31 +1,33 @@
-import keyboard
+# import keyboard
 
 
-listOfNumbers = []
-askForInput = True
+# listOfNumbers = []
+# askForInput = True
 
 
-def askForNumbers():
-    inputNumber = input("Give me some arbituary numbers (space bar to terminate):   ")
-    listOfNumbers.append(int(inputNumber))
+# def askForNumbers():
+#     inputNumber = input("Give me some arbituary numbers (space bar to terminate):   ")
+#     listOfNumbers.append(int(inputNumber))
 
-def askForArray():
-    inputArray = input("Give me the array waiting to be sorted:   ")
-    listOfNumbers = inputArray
+# def askForArray():
+#     inputArray = input("Give me the array waiting to be sorted:   ")
+#     listOfNumbers = inputArray
 
     
-if askForInput:
-    numberOrArray = input("Numbers or Array? (1 for numbers / 2 for array):   ")
-    if numberOrArray == 1:
-        while askForInput:
-            askForNumbers()
-            if keyboard.is_pressed("q"):
-                break
+# if askForInput:
+#     numberOrArray = input("Numbers or Array? (1 for numbers / 2 for array):   ")
+#     if numberOrArray == 1:
+#         while askForInput:
+#             askForNumbers()
+#             if keyboard.is_pressed("q"):
+#                 break
 
-    elif numberOrArray == 2:
-        while askForInput:
-            askForArray()
+#     elif numberOrArray == 2:
+#         while askForInput:
+#             askForArray()
 
 
 
-print(listOfNumbers)
+# print(listOfNumbers)
+
+print('4' + '5' + '1')
